770af0af6c3f4becbcb22f219a5d3b8bced3ad9a
yurim-hgr <dbfla4618@naver.com>
2023-10-10 19:48:10 +0900

    styled-component 연습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import styled ,{keyframes} from "styled-components";
+const Father = styled.div`
+  display: flex;
+`;
+
+
+const rotateAnimation = keyframes`
+  0%{
+    transform: rotate(0deg);
+    border-radius:0px;
+  }
+  50%{
+    border-radius:100px;
+  }
+  100%{
+    transform: rotate(360deg);
+    
+    border-radius:0px;
+  }
+`
+
+const Emoji = styled.span`
+  font-size: 36px;
+  `;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgcolor};
+  width: 100px;
+  height: 100px;
+  animation: ${rotateAnimation} 1s linear infinite;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  ${Emoji}:hover {
+    font-size: 55px;
+  }
+  
+  
+  
+`;
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+const Btn = styled.button`
+
+  background-color: tomato;
+  border: 0;
+  border-radius : 15px;
+  color: white;
+  `;
+
+const Input = styled.input.attrs({required:true})`
+  background-color: pink;
+`
+
+const Title = styled.h1`
+  color: ${props => props.theme.textColor};
+  `;
+
+const Wrapper = styled.div`
+ display: flex;
+ height: 100vh;
+ width: 100ww;
+ justify-content: center;
+ align-items: center;
+ background-color: ${props => props.theme.backgroundColor};
+ `
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      
+      <Title>Hello</Title>
+    </Wrapper>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from "react-dom/client";
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const darkTheme = {
+  textColor :"whitesmoke",
+  backgroundColor:"#111"
+}
+
+const whiteTheme = {
+  textColor :"#111",
+  backgroundColor:"whitesmoke",
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
