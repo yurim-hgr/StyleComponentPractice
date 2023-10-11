cf2fca19c2f4e6916148fd2fcd155f7432613ef3
yurim-hgr <dbfla4618@naver.com>
2023-10-11 15:43:18 +0900

    Form - typeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import styled ,{keyframes} from "styled-components";
 import Circle from "./Circle";
+import { useState } from "react";
 
 
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const {currentTarget : {value}} = e;
+    console.log(value)
+    setValue(value);
+  }
+
+  const onSubmit = (e : React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("hello"+ value)
+  }
   return (
     <>
-      <Circle bgcolor="tomato" border="black"/>
-      <Circle bgcolor="teal"/>
+      <form onSubmit={onSubmit}>
+        <input type="text" placeholder="username" onChange={onChange} value={value}>
+        </input>
+        <button>Login</button>
+      </form>
     </>
   );
 }
